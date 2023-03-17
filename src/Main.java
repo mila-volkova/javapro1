ce3a8594b4c9804cc6488989ca5d198eec629f27
Neto test <lyudmila.volkova.70@mail.ru>
2023-03-17 15:15:36 +0800

    Gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 14500; //стоимость одного билета
+
+        int mile = price / 20;//расчет бонусов//
+        System.out.println("Расчет бонусов" + mile);}
+
+        }
+
