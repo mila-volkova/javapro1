3d7136050d6cd0a596dc03a1dc45f89a16aab986
Neto test <lyudmila.volkova.70@mail.ru>
2023-03-17 21:48:21 +0800

    gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price = 14500; //стоимость одного билета
+        int price = 14500;//стоимость одного билета
 
-        int mile = price / 20;//расчет бонусов//
-        System.out.println("Расчет бонусов" + mile);}
+        int mile = price / 20;//расчет бонусов
+        System.out.println("Бонусы: " + mile);
+    }
 
-        }
+}
 
